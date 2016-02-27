8237b67ffe900798342c378f855541d3055363c9
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-27 18:58:56 +0000

    App cleaned up

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,9 +5,5 @@ require_relative 'models/message'
 
 DataMapper.setup(:default, "postgres://localhost/message_board_development")
 
-# configure :production do
-#     DataMapper.setup(:default, 'postgres://nqoyarhobrdyzw:9EuarO7Nw2is6Broj_cYyQV0wf@ec2-54-83-198-159.compute-1.amazonaws.com:5432/dd8utsnouph1t4')
-# end
-
 DataMapper.finalize
 DataMapper.auto_upgrade!
